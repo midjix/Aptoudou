0972ffd384e82ab96e1b46be3ab64446e5fb5ed1
Shadhau <bonzanini.ryan@gmail.com>
2024-05-16 14:50:14 +0200

    mise a jour de tout

--- index.php
+++ index.php
@@ -10,24 +10,28 @@
     </head>
     <body>
         <header>
-            <img src="logo_utilisateur.jpg" alt="afiicher logo utilisateur" class="logo">
-            <p><h1>Utilisateur</h1></p>
+            <img src="utilisateur.png" alt="afiicher logo utilisateur" class="logo">
         </header>
         <main>
-            <form action="validation.php">
-                <p>
-                    <label for="mail">Identifiant d'utilisateur : </label>
-                    <input type="mail" id="nom" name="nom"><br>
-                </p>
-                <p>
-                    <label for="code">Code d'idenfication : </label>
-                    <input type="text" id="code" name="code"><br>
-                </p>
-            </form>
-            <p>
-                <a href="validation.php"><button>Se connecter</button></a>
-                <a href="creation_compte.php"><button>Créer un compte</button></a>
-            </p>
+            <section>
+                <article>
+                <p><h1>Utilisateur</h1></p>
+                    <form action="validation.php">
+                        <p>
+                            <label for="mail">Identifiant d'utilisateur : </label>
+                            <input type="mail" id="nom" name="nom"><br>
+                        </p>
+                        <p>
+                            <label for="code">Code d'idenfication : </label>
+                            <input type="text" id="code" name="code"><br>
+                        </p>
+                    </form>
+                    <p>
+                        <a href="validation.php"><button>Se connecter</button></a>
+                        <a href="creation_compte.php"><button>Créer un compte</button></a>
+                    </p>
+                </article>
+            </section>
         </main>
     </body>
 </html>
