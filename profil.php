0f909140dac90f420f74a5bba3d9fd16b4181036
Benjamin Chassier <benjamin.chassier@gmail.com>
2024-05-16 23:06:35 +0200

    changement pour profil ajout des bouton follow unfollow

---
+++ profil.php
@@ -0,0 +1,4 @@
+<?php
+echo '<a href="follow.php?id='. $membre['user_id'].'"><button>Follow</button></a>';
+echo '<a href="unfollow.php?id='. $membre['user_id'].'"><button>Unfollow</button></a>';
+?>
