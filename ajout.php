c9f0471a1cdcb072b18f3ad4dc3e8c4cb4e6583d
Benjamin Chassier <benjamin.chassier@gmail.com>
2024-05-16 22:02:27 +0200

    changement ajout

--- ajout.php
+++ ajout.php
@@ -6,9 +6,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nom = $_POST['nom'];
     $mdp = $_POST['mdp'];
 
-    $ajouter_utilisateur = $connexion->prepare("INSERT INTO utilisateur (mail, nom, mdp) VALUES ($mail, $nom, $mdp)");
-    $ajouter_utilisateur->execute();
-    
+    $statement = $connexion->prepare("INSERT INTO utilisateur (mail, nom, mdp) VALUES (?, ?, ?)");
+    $statement->execute([$mail, $nom, $mdp]);
     header("Location: index.php");
     exit();
 }
