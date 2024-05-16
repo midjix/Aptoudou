2a5c6d23bf742f820dc9192d4b9608df7cb5caec
Benjamin Chassier <benjamin.chassier@gmail.com>
2024-05-16 12:27:46 +0200

    ajustement + bouton

--- index.php
+++ index.php
@@ -15,10 +15,20 @@
         <main>
             <form action="validation.php">
                 <p>
-                    <label for="nom">Nom :</label><br>
+                    <label for="nom">Nom d'utilisateur : </label>
                     <input type="text" id="nom" name="nom"><br>
                 </p>
+                <p>
+                    <label for="code">Code d'idenfication : </label>
+                    <input type="text" id="code" name="code"><br>
+                </p>
             </form>
+            <p>
+                <a href="validation.php"><button>Se connecter</button></a>
+            </p>
+            <p>
+                <a href="creation_compte.php"><button>Créer un compte</button></a>
+            </p>
         </main>
     </body>
 </html>
