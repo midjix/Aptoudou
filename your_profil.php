10f8998012c787dab1354182814d54ce0d89ccdc
William baal <williamxp95@gmail.com>
2024-05-16 17:05:31 +0200

    Debut de travail sur your_profil.php

--- your_profil.php
+++ your_profil.php
@@ -1,11 +1,33 @@
+<?php
+require 'config.php';
+$id = $_GET['id'];
+
+$statement_utilisateur = $connexion -> prepare("SELECT * FROM utilisateur WHERE user_id = $id");
+$statement_utilisateur -> execute();
+$membres = $statement_utilisateur -> fetch();
+
+$statement_projets = $connexion -> prepare("SELECT * FROM projets WERE")
+?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <main>
+            <section>
+                <h1>
+                    bienvenue <?php echo $membre['username']?>
+                </h1>
+            </section>
+            <section>
+                <h2>votre liste de projets : </h2>
+                <p>
+                    <?php foreach() ?>
+                </p>
+            </section>
+        </main>
+    </body>
 </html>
