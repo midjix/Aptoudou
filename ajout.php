4b6633f8c2080e119e5361fbec5764f4b11a311f
William baal <williamxp95@gmail.com>
2024-05-16 22:06:58 +0200

    nom ==> username

--- ajout.php
+++ ajout.php
@@ -6,7 +6,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nom = $_POST['nom'];
     $mdp = $_POST['mdp'];
 
-    $statement = $connexion->prepare("INSERT INTO utilisateur (mail, nom, mdp) VALUES (?, ?, ?)");
+    $statement = $connexion->prepare("INSERT INTO utilisateur (mail, username, mdp) VALUES (?, ?, ?)");
     $statement->execute([$mail, $nom, $mdp]);
     header("Location: index.php");
     exit();
