78630743d55f4d380b61a25815cfbb00d7fbdaf2
Shadhau <bonzanini.ryan@gmail.com>
2024-05-17 11:23:08 +0200

    php

--- community.php
+++ community.php
@@ -13,16 +13,17 @@ $membres = $statement_utilisateur -> fetchAll();
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Community</title>
+        <link rel="stylesheet" href="community.css">
     </head>
     <body>
+        <p><h1>Community</h1></p>
         <header>
-            <p><h1>Community</h1></p>
+            <img src="utilisateur1.png" alt="afiicher logo utilisateur" class="logo">
         </header>
         <main>
             <?php
             foreach($membres as $membre){
                 echo "<section>";
-                echo "<img src='logo_utilisateur.jpg' alt='afiicher logo utilisateur'>";
                 echo '<a href="profil.php?id='. $membre['user_id'].'">'. $membre['username'] . '</a>';
                 echo "</section>";
             }
