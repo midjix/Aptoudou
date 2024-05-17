e193704787bbabc03cb965fe03b1060c7633b8bd
William baal <williamxp95@gmail.com>
2024-05-17 12:53:55 +0200

    erreur validation retour index.php

--- validation.php
+++ validation.php
@@ -5,25 +5,21 @@
    if(isset($_POST['mail'])){
         $mail = $_POST['mail'];
         $mdp = $_POST['mdp'];
-        try {
-            $statement = $connexion -> prepare("SELECT * FROM utilisateur WHERE mail = ? ");
-            $statement -> execute([$mail]);
+    
+        $statement = $connexion -> prepare("SELECT * FROM utilisateur WHERE mail = ? ");
+        $statement -> execute([$mail]);
 
-            $utilisateur = $statement -> fetch();
+        $utilisateur = $statement -> fetch();
 
-            if ($mdp == $utilisateur['mdp']){
-                $_SESSION['user_id'] = $utilisateur['user_id'];
-                header("Location: your_profil.php");
-                exit();
-            } else { 
-                header("Location: index.php?message=1");
-                exit();
-            }
-
-        } catch (\Throwable $th) {
+        if ($mdp == $utilisateur['mdp']){
+            $_SESSION['user_id'] = $utilisateur['user_id'];
+            header("Location: your_profil.php");
+            exit();
+        } else { 
             header("Location: index.php?message=1");
             exit();
-        }     
+        }
+    
    }
 ?>
 
