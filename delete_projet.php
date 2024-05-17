e043a7bd1651cd4840671fdbe5c5da7df3d90c86
William baal <williamxp95@gmail.com>
2024-05-17 14:30:41 +0200

    changement delete

--- delete_projet.php
+++ delete_projet.php
@@ -1,11 +1,11 @@
 <?php
 require 'config.php';
 
-if(isset($_GET['id'])){
-    $projet_ID = $_POST['id'];
+if(isset($_POST['projet_ID'])){
+    $projet_ID = $_POST['projet_ID'];
 
-    $statement = $connexion->prepare("DELETE FROM projets WHERE projet_ID = $projet_ID");
-    $statement->execute();
+    $statement = $connexion->prepare("DELETE FROM projets WHERE projet_ID = ?");
+    $statement->execute([$projet_ID]);
 
     header("Location: your_profil.php");
     exit();
