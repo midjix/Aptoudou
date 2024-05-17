decf566115a69fcb2d28312ab5c7b5c3056940fd
Shadhau <bonzanini.ryan@gmail.com>
2024-05-17 12:39:59 +0200

    css

--- ajout_projet.php
+++ ajout_projet.php
@@ -6,7 +6,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $description = $_POST['description'];
     $nom = $_POST['nom'];
     $is_done = $_POST['is_done'];
-    $user_id = $_SESSION['user_id']
+    $user_id = $_SESSION['user_id'];
 
     $statement = $connexion->prepare("INSERT INTO projet (nom, is_done, description, user_id) VALUES (?, ?, ?, ?)");
     $statement->execute([$nom, $is_done, $description, $user_id]);
